07233ebdd7ca8ff1e8fd37a0146c4542a84ac384
Nazar Dzhura <nazar_dzhura@ukr.net>
2021-11-18 17:17:58 +0200

    add: lab2

---
+++ lab2/MySQL_DB_lab2_v8.sql
@@ -0,0 +1,85 @@
+-- 1.Отримати інформацію про комп’ютери, що
+-- мають частоту процесора не менше 500 МГц та ціну нижче 800 дол.
+-- Вихідні дані впорядкувати за спаданням за стовпцем price.
+SELECT * FROM pc 
+	WHERE speed >= 500 AND price < 800 
+	ORDER BY price DESC;
+    
+-- 2. Вивести всі моделі ПК, у номерах яких є хоча б
+-- дві одинички.    
+SELECT * FROM pc WHERE model LIKE '%1%1%';
+
+-- 3. Для кораблів таблиці Ships вивести їх водотоннаж-
+-- ність.
+SELECT name, displacement FROM ships
+	JOIN classes ON ships.class = classes.class;
+
+-- 4. Знайти моделі та ціни ноутбуків, вартість яких є
+-- вищою за вартість будь-якого ПК. Вивести: model, price.
+SELECT * FROM laptop
+	WHERE price > (SELECT MAX(price) FROM pc);
+    
+-- 5. Знайдіть виробників, які б випускали ноутбуки
+-- зі швидкістю 500 МГц та нижче. Виведіть: maker.    
+SELECT p.maker FROM laptop l 
+	LEFT JOIN product p ON l.model = p.model
+	WHERE l.speed <= 500;
+    
+-- 6. Вивести значення таблиці Ships із коментарями,
+-- наприклад, 'name: California', 'class: Tennessee', 'launched: 1921'.    
+SELECT * FROM ships
+	WHERE name = 'California' AND class = 'Tennessee' AND launched = '1921';
+
+-- 7. Визначіть дні, коли було виконано максимальну
+-- кількість рейсів із міста 'Rostov'. Вивести: date, число рейсів.
+SELECT time_out, COUNT(*) AS race_count FROM trip
+	WHERE town_from ='Rostov'
+    GROUP BY town_from
+    HAVING MAX(race_count);
+    
+-- 8. Знайдіть виробників, які б випускали ноутбуки з
+-- мінімальною швидкістю не менше 600 МГц. Вивести: maker,
+-- мінімальна швидкість. (Підказка: використовувати підзапити в якості
+-- обчислювальних стовпців)
+SELECT p.maker FROM laptop l
+	LEFT JOIN product p ON l.model = p.model 
+	WHERE l.speed >= 600;
+    
+-- 9. Визначити назви всіх кораблів із таблиці Ships, які
+-- задовольняють, у крайньому випадку, комбінації будь-яких чотирьох
+-- критеріїв із наступного списку: numGuns=8, bore=15,
+-- displacement=32000, type='bb', country='USA', launched=1915,
+-- class='Kongo'. Вивести: name, numGuns, bore, displacement, type,
+-- country, launched, class. (Підказка: використати для перевірки умов
+-- оператор CASE)
+SELECT name, numGuns, bore, displacement, type, country, launched, classes.class FROM ships 
+	JOIN classes ON ships.class = classes.class
+    WHERE
+		CASE WHEN numGuns = 8 THEN 1 ELSE 0 END +
+		CASE WHEN bore = 15 THEN 1 ELSE 0 END +
+		CASE WHEN displacement = 32000 THEN 1 ELSE 0 END +
+		CASE WHEN type = 'bb' THEN 1 ELSE 0 END +
+		CASE WHEN launched = 1915 THEN 1 ELSE 0 END +
+		CASE WHEN ships.class = 'Kongo' THEN 1 ELSE 0 END +
+		CASE WHEN country = 'USA' THEN 1 ELSE 0 END >= 4;
+
+-- 10. Знайдіть класи, у які входять лише один або два
+-- кораблі із цілої БД (врахувати також кораблі в таблиці Outcomes, яких
+-- немає в таблиці Ships). Вивести: class, кількість кораблів у класі.
+-- (Підказка: використовувати оператор UNION та операцію EXISTS)
+SELECT class, 
+(SELECT COUNT(*)
+FROM Ships
+WHERE Ships.class=Classes.class)
+AS numOfShipsInClass
+FROM Classes
+WHERE EXISTS(SELECT * FROM Ships WHERE Ships.class=Classes.class)
+UNION SELECT class,
+(SELECT COUNT(*)
+FROM Outcomes
+WHERE Classes.class = Outcomes.ship
+) AS numOfShipsInClass
+FROM Classes
+WHERE EXISTS(SELECT * FROM Outcomes WHERE Classes.class = Outcomes.ship)
+AND NOT EXISTS(SELECT * FROM Ships WHERE Ships.class=Classes.class)
+
---
+++ lab2/README.md
@@ -0,0 +1,32 @@
+# lab-2 task
+
+1. DB "Comp. firm". Get information about computers that
+have a processor frequency of at least 500 MHz and a price below $ 800.
+Sort the output in descending order by price column.
+2. DB "Comp. firm". List all PC models that have at least the numbers
+two units.
+3. DB "Ships". For ships of the Ships table to deduce their tonnage
+ness.
+4. DB "Comp. firm". Find the models and prices of laptops that are available
+higher than the cost of any PC. Output: model, price.
+5. DB "Comp. firm". Find manufacturers who would produce laptops
+at speeds of 500 MHz and below. Print: maker.
+6. DB "Ships". Display the values ​​of the Ships table with comments,
+for example, 'name: California', 'class: Tennessee', 'launched: 1921'.
+7. DB "Airport". Determine the days when the maximum was met
+number of flights from Rostov. Output: date, number of flights.
+8. DB "Comp. firm". Find manufacturers who would produce laptops from
+at a minimum speed of at least 600 MHz. Print: maker,
+minimum speed. (Hint: use subqueries as
+computational columns)
+9. DB "Ships". Identify the names of all ships from the Ships table that
+satisfy, as a last resort, combinations of any four
+criteria from the following list: numGuns = 8, bore = 15,
+displacement = 32000, type = 'bb', country = 'USA', launched = 1915,
+class = 'Congo'. Print: name, numGuns, bore, displacement, type,
+country, launched, class. (Hint: use to check conditions
+CASE operator)
+10. DB "Ships". Find classes that include only one or two
+ships from the whole database (also consider ships in the Outcomes table, which
+not in the Ships table). Output: class, number of ships in the class.
+(Hint: use UNION statement and EXISTS operation)
